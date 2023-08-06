0d85fdd6174669b25f54966d3af521f23e767c89
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-06 22:14:57 +0300

    feat: field logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module returns log message
+Message Logger
 """
 import logging
 import re
@@ -10,28 +10,9 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
-    returns the log message obfuscated
+    Returns the log message obfuscated.
     """
     for field in fields:
-        pattern = r"(?<={}=)[\w*\d*\/.@-]+(?={})".format(field, separator)
+        pattern = r"(?<={}=)[\w\d\/.@-]+(?={})".format(field, separator)
         message = re.sub(pattern, redaction, message)
     return message
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """ Filters values incoming log records using filter_datum """
-        msg = super().format(record)
-        msg = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
-        return msg
