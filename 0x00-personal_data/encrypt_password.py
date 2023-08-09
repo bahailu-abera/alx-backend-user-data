19250cad95519194fdc893a691a52fa917652968
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-09 22:28:15 +0300

    feat: annotate the method

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,9 +3,10 @@
 Module for encrypting password
 """
 import bcrypt
+from typing import ByteString
 
 
-def hash_password(password) -> str:
+def hash_password(password) -> ByteString:
     """
     Returns a salted, hashed password, which is a byte string.
     """
