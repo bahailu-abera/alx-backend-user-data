ee2a36cbb34c926478f677e8a31ab8358e5899d3
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-09 22:41:16 +0300

    feat: confirm password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,7 +5,7 @@ Module for encrypting password
 import bcrypt
 
 
-def hash_password(password) -> bytes:
+def hash_password(password: str) -> bytes:
     """
     Returns a salted, hashed password, which is a byte string.
     """
@@ -13,6 +13,13 @@ def hash_password(password) -> bytes:
     salt = bcrypt.gensalt()
 
     # Hash the password with the salt
-    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check if the provided password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
