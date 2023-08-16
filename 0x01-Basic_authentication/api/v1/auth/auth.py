d3a8f4b878c39e2dea161f75e31a13877dfac004
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-16 21:02:34 +0300

    document the methods and the class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,20 +7,26 @@ from typing import List
 
 
 class Auth:
+    """
+    Authentication module for the api.
+    """
     def __init__(self):
         pass
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
+        Checks if the end point is required to authenticate.
         """
         return False
 
     def authorization_header(self, request=None) -> str:
         """
+        Returns the Authorization header.
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
+        Returns the current active user.
         """
         return None
