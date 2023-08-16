350388e7aee117cf736b6dc18112a8111361e830
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-16 21:38:37 +0300

    auth requirement check

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,6 +14,14 @@ class Auth:
         """
         Checks if the end point is required to authenticate.
         """
+        if not path or not excluded_paths:
+            return True
+
+        path_with_slash = path if path.endswith('/') else path + '/'
+
+        if path_with_slash in excluded_paths:
+            return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
