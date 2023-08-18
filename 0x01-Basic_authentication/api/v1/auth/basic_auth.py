2c5ea583d13822d2557f01de05b9398270eaff8c
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-18 21:13:36 +0300

    correct: pycodestyle violations

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,12 +13,15 @@ class BasicAuth(Auth):
     def __init__(self) -> None:
         super().__init__()
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """
-        Returns the Base64 part of the Authorization header for a Basic Authentication
+        Returns the Base64 part of the Authorization
+        header for a Basic Authentication
         """
-        if authorization_header is None or not isinstance(authorization_header, str) \
-            or not authorization_header.startswith('Basic '):
+        if authorization_header is None \
+           or not isinstance(authorization_header, str) \
+           or not authorization_header.startswith('Basic '):
             return None
 
         return " ".join(authorization_header.split(' ')[1:])
