82a2bea2681e329e1ef766eb58e5ed9a9c00267c
bahailu-abera <aberabahailu4@gmail.com>
2023-08-18 18:59:10 +0000

    get credential

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -40,3 +40,16 @@ class BasicAuth(Auth):
 
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Returns the user email and password from the Base64 decoded value.
+        """
+        if decoded_base64_authorization_header is None or \
+           not isinstance(decoded_base64_authorization_header, str) or \
+           not ':' in decoded_base64_authorization_header:
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':')
+
+        return email, password
