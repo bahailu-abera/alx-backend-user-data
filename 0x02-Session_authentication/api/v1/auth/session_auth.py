d979a1430f020bdd05b3626000ac5b7b0c8653f6
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-19 16:27:46 +0300

    feat: get user id for the given session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -22,8 +22,17 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
 
-        session_id = uuid4()
+        session_id = str(uuid4())
 
-        self.user_id_by_session_id[session_id] = user_id
+        SessionAuth.user_id_by_session_id[session_id] = user_id
 
-        return session_id
+        return str(session_id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a User ID based on a Session ID
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return SessionAuth.user_id_by_session_id.get(session_id)
