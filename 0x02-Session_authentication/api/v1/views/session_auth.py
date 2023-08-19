8ee0a6fa1f71775211979b744a93524a8cde2c15
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-19 17:45:17 +0300

    fix: change the method to post

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,7 +4,7 @@ from flask import jsonify, request
 from api.v1.views import app_views
 
 
-@app_views.route('/auth_session/login', methods=['GET'], strict_slashes=False)
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
     """
     Authenticate a user for login.
