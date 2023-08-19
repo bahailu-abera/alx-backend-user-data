fb180e556f53dc8641a4ec948dc08fc676a8f9f1
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-19 16:18:43 +0300

    fix: cast uuid4 to string

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -22,7 +22,7 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
 
-        session_id = uuid4()
+        session_id = str(uuid4())
 
         self.user_id_by_session_id[user_id] = session_id
 
