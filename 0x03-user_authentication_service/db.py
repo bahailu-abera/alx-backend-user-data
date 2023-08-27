e911a9b5876e7b7e5013d417dcdb41b3b70edb6b
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-27 23:20:09 +0300

    fix: correct module imports

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import ArgumentError, InvalidRequestError, NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -41,7 +42,7 @@ class DB:
 
         return self._session.query(User).filter_by(email=email).first()
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """
         Returns the first row found in the users table as filtered by
         the method's input arguments.
