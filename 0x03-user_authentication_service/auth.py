065ca46f95efee2404cf794e77873e57ee5db4dc
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-29 23:21:47 +0300

    feat: get use by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,3 +74,19 @@ class Auth:
 
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """
+        Fetchs the user using session_id
+        """
+
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+
+            return user.session_id
+
+        except NoResultFound:
+            return None
